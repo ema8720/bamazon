9e02d10d04c714e916069921f857bd1cdaee3da4
Juan Correa <juancorrea@Juans-MacBook-Pro.local>
2018-03-03 11:47:29 -0800

    bamazonManagerworking

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -62,3 +62,4 @@ var promptCustomer = function(res) {
         }
     })
 }
+``
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,9 +10,9 @@ var connection = mysql.createConnection({
 })
 
 var makeTable = function() {
-    connection.query("SELECT * FROM product", function(err, res) {
+    connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log("ItemID\tDepartment Name\tPrice\tNumber in Stock");
+        console.log("ItemID\tPoduct Name\tDepartment Name\tPrice/tNumber in Stock");
         console.log("-----------------------------");
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].itemid + "\t" + res[i].productname + "\t" + res[i].departmentname + "\t" + res[i].price + "\t" + res[i].stockquantity);
